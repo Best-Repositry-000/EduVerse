f837b0c6a8aa4aeba18e5fe7c1e02b4bdb7697d3
Best Idodo <222670908+Best-Repositry-000@users.noreply.github.com>
2025-12-17 13:45:37 +0100

    Initial commit

---
+++ src/test/java/com/example/eduverse/EduVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eduverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
