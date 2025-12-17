87e3ed6a35521b937db1373e76b85ceae9ff051e
Best Idodo <222670908+Best-Repositry-000@users.noreply.github.com>
2025-12-17 13:47:55 +0100

    Initial commit

--- src/main/java/com/example/eduverse/EduVerseApplication.java
+++ src/main/java/com/example/eduverse/EduVerseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EduVerseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EduVerseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EduVerseApplication.class, args);
+    }
 
 }
